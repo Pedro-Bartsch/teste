3b2de4201d930221f505b13fad29e148cceec360
Pedro-Bartsch <pedro_bartsch-silva@estudante.sesisenai.org.br>
2024-08-26 15:52:05 -0300

    vcvghbjb

---
+++ atividades/atividades_19.08/Atividade-01.py
@@ -0,0 +1,10 @@
+soma = 0
+for i in range(1, 51):
+    soma += i
+    print("Soma: ", soma)
+
+# A primeira linha soma = 0 define a variável soma como 0 para que não possua nenhum valor anterior.
+# A segunda linha faz o laço contar de 1 até 50
+# A terceira linha faz a soma receber mais i, não vai até 51 por conta do +=
+# A quarta linha escreve a declarando que o valor da soma é os números do comando for são mostrados.
+ 
